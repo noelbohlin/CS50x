3f97a19ecebf4531ee7e2c98e23ea2d406fe08d9
noelbohlin <bohlin.noel@gmail.com>
2024-01-26 11:09:27 +0100

    commit

---
+++ pset1/credit/credit.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+int main(void)
+{
+    long credit_card_number = get_long("Number: ");
+
+    printf("%li\n", credit_card_number);
+
+    //  calculate checksum
+
+    int digit;
+
+    while (credit_card_number != 0)
+        {
+            digit = credit_card_number % 10;
+
+            
+
+        }
+
+
+
+
+
+
+
+
+    // check brand
+
+
+
+
+}
