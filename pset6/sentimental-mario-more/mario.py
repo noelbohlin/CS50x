cdcfe875989ece00c0d462e63a508ebe9cac2206
noelbohlin <bohlin.noel@gmail.com>
2024-03-27 20:59:48 +0100

    mario

--- pset6/sentimental-mario-more/mario.py
+++ pset6/sentimental-mario-more/mario.py
@@ -4,31 +4,31 @@ while True:
     try:
         height = int(input("Height: ").strip())
 
-        if 0 < height and height < PYRAMIDHEIGHT:
+        if 0 < height < PYRAMIDHEIGHT:
             break
         continue
     except ValueError:
         continue
 
-row = 0
+row: int = 0
 while row < height:
 
-    cursorplacement = 0
+    cursorplacement: int = 0
     while cursorplacement < height:
 
         if row + cursorplacement < height - 1:
-            print(" ", end=(""))
+            print(" ", end="")
         else:
-            print("#", end=(""))
+            print("#", end="")
 
         cursorplacement += 1
 
-    print("  ", end=(""))
+    print("  ", end="")
 
-    cursorplacementright = 0
+    cursorplacementright: int = 0
     while cursorplacementright < (row + 1):
 
-        print("#", end=(""))
+        print("#", end="")
         cursorplacementright += 1
 
     print()
